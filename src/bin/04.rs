fd4869c6bf4e4d049ea542239bffc680262c3133
Lawand Othman <lwnd@pm.me>
2023-12-04 18:13:02 +0000

    part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -25,7 +25,41 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let cards: Vec<(Vec<u32>, Vec<u32>)> = input
+        .lines()
+        .map(|line| {
+            let numbers_part = line.split_once(": ").unwrap().1;
+            let parts: Vec<&str> = numbers_part.split(" | ").collect();
+            let winning_numbers: Vec<u32> = parts[0]
+                .split_whitespace()
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect();
+            let player_numbers: Vec<u32> = parts[1]
+                .split_whitespace()
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect();
+            (winning_numbers, player_numbers)
+        })
+        .collect();
+
+    let mut result = 0;
+    let mut to_process: Vec<(usize, usize)> = (0..cards.len()).map(|i| (i, 1)).collect();
+
+    while let Some((card_idx, copies)) = to_process.pop() {
+        result += copies;
+        let (ref winning_numbers, ref player_numbers) = cards[card_idx];
+        let matches = player_numbers
+            .iter()
+            .filter(|&n| winning_numbers.contains(n))
+            .count();
+
+        if card_idx + matches < cards.len() {
+            for next_card in card_idx + 1..=card_idx + matches {
+                to_process.push((next_card, copies));
+            }
+        }
+    }
+    Some(result as u32)
 }
 
 #[cfg(test)]
@@ -41,6 +75,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
