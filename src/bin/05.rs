07d454ec069ece6c7fffddd074c3180bfd2fbdd1
Lawand Othman <lwnd@pm.me>
2023-12-05 23:58:26 +0000

    part 2 - slow af tho

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -7,7 +7,21 @@ struct ConversionMap {
     length: i64,
 }
 
-fn parse_input(input: &str) -> (Vec<i64>, Vec<Vec<ConversionMap>>) {
+fn convert_number(num: i64, map: &[ConversionMap]) -> i64 {
+    map.iter()
+        .find(|conversion| {
+            num >= conversion.source_start && num < conversion.source_start + conversion.length
+        })
+        .map_or(num, |conversion| {
+            conversion.destination_start + (num - conversion.source_start)
+        })
+}
+
+fn convert_seed_to_location(seed: i64, maps: &[Vec<ConversionMap>]) -> i64 {
+    maps.iter().fold(seed, |acc, map| convert_number(acc, map))
+}
+
+pub fn part_one(input: &str) -> Option<i64> {
     let mut maps = Vec::new();
     let mut current_map = Vec::new();
     let mut seeds = Vec::new();
@@ -41,26 +55,6 @@ fn parse_input(input: &str) -> (Vec<i64>, Vec<Vec<ConversionMap>>) {
         maps.push(current_map);
     }
 
-    (seeds, maps)
-}
-
-fn convert_number(num: i64, map: &[ConversionMap]) -> i64 {
-    map.iter()
-        .find(|conversion| {
-            num >= conversion.source_start && num < conversion.source_start + conversion.length
-        })
-        .map_or(num, |conversion| {
-            conversion.destination_start + (num - conversion.source_start)
-        })
-}
-
-fn convert_seed_to_location(seed: i64, maps: &[Vec<ConversionMap>]) -> i64 {
-    maps.iter().fold(seed, |acc, map| convert_number(acc, map))
-}
-
-pub fn part_one(input: &str) -> Option<i64> {
-    let (seeds, maps) = parse_input(input);
-
     let lowest_location = seeds
         .iter()
         .map(|&seed| Some(convert_seed_to_location(seed, &maps)))
@@ -70,8 +64,51 @@ pub fn part_one(input: &str) -> Option<i64> {
     lowest_location
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut maps = Vec::new();
+    let mut current_map = Vec::new();
+    let mut seeds = Vec::new();
+    let mut is_seed = true;
+
+    for line in input.lines() {
+        if line.ends_with("map:") {
+            is_seed = false;
+            if !current_map.is_empty() {
+                maps.push(current_map);
+                current_map = Vec::new();
+            }
+        } else if !line.is_empty() {
+            let parts: Vec<i64> = line
+                .split_whitespace()
+                .filter_map(|s| s.parse().ok())
+                .collect();
+            if is_seed {
+                for chunk in parts.chunks(2) {
+                    if chunk.len() == 2 {
+                        let (start, length) = (chunk[0], chunk[1]);
+                        seeds.extend(start..start + length);
+                    }
+                }
+                is_seed = false;
+            } else if parts.len() == 3 {
+                current_map.push(ConversionMap {
+                    destination_start: parts[0],
+                    source_start: parts[1],
+                    length: parts[2],
+                });
+            }
+        }
+    }
+
+    if !current_map.is_empty() {
+        maps.push(current_map);
+    }
+
+    seeds
+        .iter()
+        .map(|&seed| Some(convert_seed_to_location(seed, &maps)))
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -87,6 +124,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }
